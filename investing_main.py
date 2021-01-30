08ec788e523d8651dbff3febcd29a0f8ef8c0bd4
Jarryd Cheso <jarryd.cheso@gmail.com>
2021-01-30 21:42:30 +0800

    Bugfixes & added strategy to find short squeeze

--- investing_main.py
+++ investing_main.py
@@ -1,25 +1,36 @@
-import investing_data
+'''
+@author: Cheso7
+TODO: Fix magnitude of info_filter 
+'''
 import investing_strategy
+import data_collection
 
 #Use this to run with chosen tickers
-tickers = ["MMM","AXP","AAPL"]
+tickers = ["AMD","NVDA","AAPL",'GME', 'AME', 'BB']
 
 #Use this to import a list of tickers from an index on Wikipedia
-# tickers = 'http://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+# tickers_list = 'http://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+# tickers = data_collection.get_tickers(tickers_list)
 
 #Parameters
 years = 5 # years of data to collect
 interval = '1mo'
 
+def main():
+    # ohlc = data_collection.ohlc(tickers, years, interval)
 
-# tickers = investing_data.get_tickers(tickers)
-# ohlc = investing_data.ohlc(tickers, years, interval)
-piotroski = investing_strategy.piotroski(tickers)
-# magic = investing_strategy.magic_formula(tickers)
+    # financials_cy, financials_py, financials_py2 = data_collection.get_financials(tickers)
+    statistics_dir_cy = data_collection.get_statistics(tickers)
 
-'''
-1. Function to scrape company data in a uniform way for current year, previous 2 years
-2. Strategy scripts for Piotroski, magic formula, portfolio rebalance, etc.
-3. Backtest Function to deploy strategies concurrently over the same timeframe
-4. Function to compare results against S&P500 and graph
-'''
+    # df_cy = data_collection.info_filter(financials_cy)
+    # df_py = data_collection.info_filter(financials_py)
+    # dy_py2 = data_collection.info_filter(financials_py2)
+    stats_cy = data_collection.info_filter_stats(statistics_dir_cy)
+
+    # piotroski = investing_strategy.piotroski(df_cy, df_py, dy_py2,tickers)
+    # magic = investing_strategy.magic_formula(df_cy, stats_cy, tickers)
+    shorted = investing_strategy.most_shorted(stats_cy, tickers)
+    return
+
+main()
+
--- investing_strategy.py
+++ investing_strategy.py
@@ -1,92 +1,66 @@
-# -*- coding: utf-8 -*-
 """
-Created on Sat Jan 23 12:04:20 2021
-
-@author: jches
+@author: Cheso7
 """
 import pandas as pd
-import investing_data
 
-def piotroski(tickers):
-# selecting relevant financial information for each stock using fundamental data
-    stats = ["Net income available to common shareholders",
-             "Total assets",
-             "Net cash provided by operating activities",
-             "Long-term debt",
-             "Other long-term liabilities",
-             "Total current assets",
-             "Total current liabilities",
-             "Common stock",
-             "Total revenue",
-             "Gross profit"] # change as required
-    
-    indx = ["NetIncome","TotAssets","CashFlowOps","LTDebt","OtherLTDebt",
-            "CurrAssets","CurrLiab","CommStock","TotRevenue","GrossProfit"]
+def most_shorted(stats_cy,tickers):
+    pcnt_short_by_float = stats_cy.iloc[24]*100
+    pcnt_short_outstanding = stats_cy.iloc[25]*100
+    no_of_shorts = stats_cy.iloc[22]
+    no_of_shares = stats_cy.iloc[19]
+    no_shares_short_outstanding = no_of_shorts*pcnt_short_outstanding
+    short_outstanding_ratio = (no_shares_short_outstanding/no_of_shares)*100 
+    df_short = pd.DataFrame([pcnt_short_by_float, 
+                            pcnt_short_outstanding, 
+                            no_shares_short_outstanding, 
+                            no_of_shares, short_outstanding_ratio], index = ['% Short by Float', 
+                                                    '% Shorts Outstanding',
+                                                    '# of Shorts Outstanding',
+                                                    '# of Shares Floated',
+                                                    'Short/Outstanding Ratio'])
+    df_short_t = df_short.transpose()
+    short_sorted = df_short_t.sort_values(by = 'Short/Outstanding Ratio', ascending=False)
+    top10_shares_short = short_sorted[:10]
+    print(top10_shares_short)
 
-    combined_financials_cy = investing_data.financials(tickers, 1)
-    combined_financials_py = investing_data.financials(tickers, 2)
-    combined_financials_py2 = investing_data.financials(tickers, 3)
+    return top10_shares_short
 
-    
-    df_cy = investing_data.info_filter(combined_financials_cy, stats, indx)
-    df_py = investing_data.info_filter(combined_financials_py, stats, indx)
-    df_py2 = investing_data.info_filter(combined_financials_py2, stats, indx)
-    
+def piotroski(df_cy, df_py, df_py2, tickers):
     f_score = {}
     tickers = df_cy.columns
     
     for ticker in tickers:
-        ROA_FS = int(df_cy.loc["NetIncome",ticker]/((df_cy.loc["TotAssets",ticker]+df_py.loc["TotAssets",ticker])/2) > 0)
-        CFO_FS = int(df_cy.loc["CashFlowOps",ticker] > 0)
-        ROA_D_FS = int(df_cy.loc["NetIncome",ticker]/(df_cy.loc["TotAssets",ticker]+df_py.loc["TotAssets",ticker])/2 > df_py.loc["NetIncome",ticker]/(df_py.loc["TotAssets",ticker]+df_py2.loc["TotAssets",ticker])/2)
-        CFO_ROA_FS = int(df_cy.loc["CashFlowOps",ticker]/df_cy.loc["TotAssets",ticker] > df_cy.loc["NetIncome",ticker]/((df_cy.loc["TotAssets",ticker]+df_py.loc["TotAssets",ticker])/2))
-        LTD_FS = int((df_cy.loc["LTDebt",ticker] + df_cy.loc["OtherLTDebt",ticker])<(df_py.loc["LTDebt",ticker] + df_py.loc["OtherLTDebt",ticker]))
-        CR_FS = int((df_cy.loc["CurrAssets",ticker]/df_cy.loc["CurrLiab",ticker])>(df_py.loc["CurrAssets",ticker]/df_py.loc["CurrLiab",ticker]))
-        DILUTION_FS = int(df_cy.loc["CommStock",ticker] <= df_py.loc["CommStock",ticker])
-        GM_FS = int((df_cy.loc["GrossProfit",ticker]/df_cy.loc["TotRevenue",ticker])>(df_py.loc["GrossProfit",ticker]/df_py.loc["TotRevenue",ticker]))
-        ATO_FS = int(df_cy.loc["TotRevenue",ticker]/((df_cy.loc["TotAssets",ticker]+df_py.loc["TotAssets",ticker])/2)>df_py.loc["TotRevenue",ticker]/((df_py.loc["TotAssets",ticker]+df_py2.loc["TotAssets",ticker])/2))
+        ROA_FS = int(df_cy.loc["Net income available to common shareholders",ticker]/((df_cy.loc["Total assets",ticker]+df_py.loc["Total assets",ticker])/2) > 0)
+        CFO_FS = int(df_cy.loc["Net cash provided by operating activities",ticker] > 0)
+        ROA_D_FS = int(df_cy.loc["Net income available to common shareholders",ticker]/(df_cy.loc["Total assets",ticker]+df_py.loc["Total assets",ticker])/2 > df_py.loc["Net income available to common shareholders",ticker]/(df_py.loc["Total assets",ticker]+df_py2.loc["Total assets",ticker])/2)
+        CFO_ROA_FS = int(df_cy.loc["Net cash provided by operating activities",ticker]/df_cy.loc["Total assets",ticker] > df_cy.loc["Net income available to common shareholders",ticker]/((df_cy.loc["Total assets",ticker]+df_py.loc["Total assets",ticker])/2))
+        LTD_FS = int((df_cy.loc["Long-term debt",ticker] + df_cy.loc["Other long-term liabilities",ticker])<(df_py.loc["Long-term debt",ticker] + df_py.loc["Other long-term liabilities",ticker]))
+        CR_FS = int((df_cy.loc["Total current assets",ticker]/df_cy.loc["Total current liabilities",ticker])>(df_py.loc["Total current assets",ticker]/df_py.loc["Total current liabilities",ticker]))
+        DILUTION_FS = int(df_cy.loc["Common stock",ticker] <= df_py.loc["Common stock",ticker])
+        GM_FS = int((df_cy.loc["Gross profit",ticker]/df_cy.loc["Total revenue",ticker])>(df_py.loc["Gross profit",ticker]/df_py.loc["Total revenue",ticker]))
+        ATO_FS = int(df_cy.loc["Total revenue",ticker]/((df_cy.loc["Total assets",ticker]+df_py.loc["Total assets",ticker])/2)>df_py.loc["Total revenue",ticker]/((df_py.loc["Total assets",ticker]+df_py2.loc["Total assets",ticker])/2))
         f_score[ticker] = [ROA_FS,CFO_FS,ROA_D_FS,CFO_ROA_FS,LTD_FS,CR_FS,DILUTION_FS,GM_FS,ATO_FS]
     f_score_df = pd.DataFrame(f_score,index=["PosROA","PosCFO","ROAChange","Accruals","Leverage","Liquidity","Dilution","GM","ATO"])
     f_score_sorted = f_score_df.sum().sort_values(ascending=False)
     
     return f_score_sorted
 
-def magic_formula(tickers):
-    stats = ["EBITDA",
-          "Depreciation & amortisation",
-          "Market Cap (intraday)",
-          "Net income available to common shareholders",
-          "Net cash provided by operating activities",
-          "Capital expenditure",
-          "Total current assets",
-          "Total current liabilities",
-          "Net property, plant and equipment",
-          "Total stockholders' equity",
-          "Long-term debt",
-          "Forward Annual Dividend Yield"] # change as required
-    
-    indx = ["EBITDA","D&A","MarketCap","NetIncome","CashFlowOps","Capex","CurrAsset",
-         "CurrLiab","PPE","BookValue","TotDebt","DivYield"] 
-     
-    combined_financials_cy = investing_data.financials(tickers, 1)
-    combined_financials_cy_dropped = combined_financials_cy.drop("EBITDA")
-    statistics = investing_data.statistics(tickers)
-    
-    all_stats_df = combined_financials_cy_dropped.append(statistics)
-    all_stats_df = investing_data.info_filter(all_stats_df, stats, indx)
+def magic_formula(df_cy, df_stats, tickers):
+
+    all_stats_df = df_cy.append(df_stats)
     
     # calculating relevant financial metrics for each stock
     transpose_df = all_stats_df.transpose()
     final_stats_df = pd.DataFrame()
-    final_stats_df["EBIT"] = transpose_df["EBITDA"] - transpose_df["D&A"]
-    final_stats_df["TEV"] =  transpose_df["MarketCap"].fillna(0) \
-                             +transpose_df["TotDebt"].fillna(0) \
-                             -(transpose_df["CurrAsset"].fillna(0)-transpose_df["CurrLiab"].fillna(0))
+    final_stats_df["EBIT"] = transpose_df["EBITDA"] - transpose_df["Depreciation & amortisation"]
+    final_stats_df["TEV"] =  transpose_df["Market cap (intra-day)"].fillna(0) \
+                             +transpose_df["Long-term debt"].fillna(0) \
+                             -(transpose_df["Total current assets"].fillna(0)-transpose_df["Total current liabilities"].fillna(0))
     final_stats_df["EarningYield"] =  final_stats_df["EBIT"]/final_stats_df["TEV"]
-    final_stats_df["FCFYield"] = (transpose_df["CashFlowOps"]-transpose_df["Capex"])/transpose_df["MarketCap"]
-    final_stats_df["ROC"]  = (transpose_df["EBITDA"] - transpose_df["D&A"])/(transpose_df["PPE"]+transpose_df["CurrAsset"]-transpose_df["CurrLiab"])
-    final_stats_df["BookToMkt"] = transpose_df["BookValue"]/transpose_df["MarketCap"]
-    final_stats_df["DivYield"] = transpose_df["DivYield"]
+    final_stats_df["FCFYield"] = (transpose_df["Net cash provided by operating activities"]-transpose_df["Capital expenditure"])/transpose_df["Market cap (intra-day)"]
+    final_stats_df["ROC"]  = (transpose_df["EBITDA"] - transpose_df["Depreciation & amortisation"])/(transpose_df["Net property, plant and equipment"]+transpose_df["Total current assets"]-transpose_df["Total current liabilities"])
+    final_stats_df["BookToMkt"] = transpose_df["Total stockholders' equity"]/transpose_df["Market cap (intra-day)"]
+    final_stats_df["Forward annual dividend yield"] = transpose_df["Forward annual dividend yield"]
     
     
     ################################Output Dataframes##############################
@@ -101,7 +75,7 @@ def magic_formula(tickers):
     print(value_stocks)
     
     # finding highest dividend yield stocks
-    high_dividend_stocks = final_stats_df.sort_values("DivYield",ascending=False).iloc[:,6]
+    high_dividend_stocks = final_stats_df.sort_values("Forward annual dividend yield",ascending=False).iloc[:,6]
     print("------------------------------------------------")
     print("Highest dividend paying stocks")
     print(high_dividend_stocks)
@@ -109,7 +83,7 @@ def magic_formula(tickers):
     # # Magic Formula & Dividend yield combined
     final_stats_df["CombRank"] = final_stats_df["EarningYield"].rank(ascending=False,method='first') \
                                   +final_stats_df["ROC"].rank(ascending=False,method='first')  \
-                                  +final_stats_df["DivYield"].rank(ascending=False,method='first')
+                                  +final_stats_df["Forward annual dividend yield"].rank(ascending=False,method='first')
     final_stats_df["CombinedRank"] = final_stats_df["CombRank"].rank(method='first')
     value_high_div_stocks = final_stats_df.sort_values("CombinedRank").iloc[:,[2,4,6,8]]
     print("------------------------------------------------")
